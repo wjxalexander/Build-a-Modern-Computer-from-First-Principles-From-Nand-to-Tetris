0f86a06caa45ed930d81c424e05372c6aafba10a
jingxin wang <wjxalexander@hotmail.com>
2020-12-12 12:01:44 +0800

    compileClassVarDec done

--- compiler/CompilationEngine.js
+++ compiler/CompilationEngine.js
@@ -1,9 +1,12 @@
 const { tokenizer } = require("./Tokenizer")
+const compilerDirectMap = require("./static/compilerHeadKeywords.json")
+
+const { classVarDecKeyWord, functionTitles } = compilerDirectMap;
+
 function main(filePath) {
     const tokenizedCode = tokenizer(filePath)
     compileClass(tokenizedCode)
 }
-
 
 function compileClass(codes) {
     const [classKeyword, className, leftBrace, ...rest] = codes
@@ -14,18 +17,55 @@ function compileClass(codes) {
     if (clsKey !== "class" || lftBrace !== "{" || rgtBrace !== "}") {
         throw new Error("wrong class format")
     }
-    return `<class>
-  <keyword> ${clsKey} </keyword>
-  <identifier> ${clsName} </identifier>
-  <symbol> { </symbol>
-  ${complileClassBody(rest)}
-  <symbol> } </symbol>
-</class>`
+    return [
+        `<class>`,
+        `  <keyword> ${clsKey} </keyword>`,
+        `  <identifier> ${clsName} </identifier>`,
+        `  <symbol> { </symbol>`,
+        ...complileClassBody(rest),
+        `  <symbol> } </symbol>`,
+        `</class>`
+    ]
 }
 
-function complileClassBody(classBody){
-
-    console.log(classBody)
+function complileClassBody(classBody) {
+    const router = {
+        varStack: [],
+        subroutine: []
+    }
+    let active = ""
+    for (const token of classBody) {
+        const { content } = token
+        if (classVarDecKeyWord.includes(content) || functionTitles.includes(content)) {
+            active = classVarDecKeyWord.includes(content) ? "varStack" : "subroutine"
+            router[active].push([token])
+            continue
+        }
+        const lastIndex = router[active].length - 1
+        router[active][lastIndex].push(token)
+    }
+    const {varStack,subroutine} = router
+    const classVarDecResult = compileClassVarDec(varStack)
+    const subroutineResulte = compileSubroutine(subroutine)
+    console.log(router)
 }
 
-main('./compiler/tests/ExpressionLessSquare/Main.jack')
+function compileClassVarDec(varDecs) {
+    const tokenizedVars = varDecs.map(item => {
+        return [
+            `<classVarDec>`,
+            ...item.map(({ tag, content }) => "  <" + `${tag}> ${content} </${tag}>`),
+            `</classVarDec>`
+        ]
+    })
+
+    return tokenizedVars
+}
+
+function compileSubroutine(routines) {
+    // const 
+    console.log(routines)
+}
+
+main('./compiler/tests/ExpressionLessSquare/Square.jack')
+// main('./compiler/tests/ArrayTest/main.jack')
--- compiler/Tokenizer.js
+++ compiler/Tokenizer.js
@@ -10,15 +10,15 @@ function tokenizer(filepath) {
     const preloadCodes = preload(filepath)
 
     const grouptedTokens = preloadCodes.map(codeHandler).reduce((acc, cur) => acc.concat(cur))
-    const tokensWithTag =  grouptedTokens.map(getTokenType)
-    const xmlPart = tokensWithTag.map(item=>item.xml)
-    const jsonPart = tokensWithTag.map(item=>item.json)
+    const tokensWithTag = grouptedTokens.map(getTokenType)
+    const xmlPart = tokensWithTag.map(item => item.xml)
+    const jsonPart = tokensWithTag.map(item => item.json)
     const tokensWithLabel = ["<tokens>", ...xmlPart, "</tokens>"]
 
     const fileArr = filepath.split('/')
     const fileName = fileArr[fileArr.length - 1].split('.')[0]
     fileArr.pop()
-    
+
     writeFile(tokensWithLabel, fileArr.join("/"), `${fileName}T_my`, "xml")
     return jsonPart
 }
@@ -97,26 +97,37 @@ function keywordAndSymbolTokenGenerate(keyword, symbol) {
     if (specialSymbolMap.hasOwnProperty(keyword)) {
         keyword = specialSymbolMap[keyword]
     }
-    return { xml: "<" + `${symbol}> ${keyword} </${symbol}>`, json: { content: keyword, tag: symbol } }
+    return {
+        xml: "<" + `${symbol}> ${keyword} </${symbol}>`,
+        json: { content: keyword, tag: symbol }
+    }
 }
 
 function integerTokenGenerater(number) {
-    return { xml: `<integerConstant> ${number} </integerConstant>`, json: { content: number, tag: "integerConstant" } }
+    return {
+        xml: `<integerConstant> ${number} </integerConstant>`,
+        json: { content: number, tag: "integerConstant" }
+    }
 }
 
 function stringTokenGenerater(string) {
     const stringCotent = string.substring(1, string.length - 1)
-    return { xml: `<stringConstant> ${stringCotent} </stringConstant>`, json: { content: stringCotent, tag: "stringConstant" } }
+    return {
+        xml: `<stringConstant> ${stringCotent} </stringConstant>`,
+        json: { content: stringCotent, tag: "stringConstant" }
+    }
 }
 function identifierTokenGenrater(identifier) {
-    return { xml: `<identifier> ${identifier} </identifier>`, json: { content: identifier, tag: "identifier" } }
+    return {
+        xml: `<identifier> ${identifier} </identifier>`,
+        json: { content: identifier, tag: "identifier" }
+    }
 
 }
 
 module.exports = {
     tokenizer
 }
-// tokenizer('./compiler/tests/ExpressionLessSquare/Main.jack')
 // tokenizer('./compiler/tests/ExpressionLessSquare/Main.jack')
 // tokenizer('./compiler/tests/ExpressionLessSquare/SquareGame.jack')
 // tokenizer('./compiler/tests/ExpressionLessSquare/Square.jack')
